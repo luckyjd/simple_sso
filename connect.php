9be67d800ef5a38ce4a60545c98e4154b1b96aa4
nhattx <nhattx@elephas.vn>
2018-01-10 14:51:44 +0700

    update connect db 50.61

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
+$servername = "simple.sso.tinhvan.com:3306";
 $username = "root";
-$password = "mysql@1211";
-$dbname = "sso_db";
+$password = "tinhvan@2017";
+$dbname = "simple_sso_db";
 
 // Create connection by MySQLi Object-oriented
 $conn = mysqli_connect($servername, $username, $password, $dbname);
