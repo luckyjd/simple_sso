527519a63919d42980bde6a3386dba20e501ba42
luckyjd <narusunday@gmail.com>
2018-01-22 14:17:42 +0700

    update

---
+++ setallcookie.php
@@ -0,0 +1,15 @@
+<html>
+   <head>
+   <script>
+   function loadComplete(){
+      window.location="http://nhattx.learnphp.tinhvan.com";
+   }
+   </script>
+</head>
+<body onload="loadComplete()">
+   <body>
+      <p>Please wait.....</p>
+      <img src="http://nhatt.learnphp.tinhvan.com/setcookie.php?theme=whateveryourthemehere" />
+      <img src="http://nhattx.sso.com.com/setcookie.php?theme=whateveryourthemehere" />
+   </body>
+</html>
