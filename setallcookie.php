46ff9c6bf86530ae9cf49cbd039e8e9175334115
luckyjd <narusunday@gmail.com>
2018-01-22 15:58:17 +0700

    update

--- setallcookie.php
+++ setallcookie.php
@@ -3,23 +3,24 @@
     $token = $_POST['token'];
     $id = $_POST['sso_id'];
     $username = $_POST['username'];
-    $url_nhat = "nhattx.learnphp.tinhvan.com/setcookie.php?" . "id=" . $id . "&username=" . $username . "&token=" . $token;
+    $url_nhat = "http://nhattx.learnphp.tinhvan.com/setcookie.php?" . "id=" . $id . "&username=" . $username . "&token=" . $token;
 ?>
 <html>
    <head>
-   
+   <script>
+   function loadComplete(){
+      window.location= $('#url_handle').val();
+   }
+   </script>
 </head>
 <body onload="loadComplete()">
-   <body>
         
         <p>Please wait.....</p>
         <input type='hidden' id='url_handle' value="<?php echo $_POST['url_handle']; ?>">
         <img src="<?php echo $url_nhat;?>" />
         <img src="" />
-        <script>
-   function loadComplete(){
-      window.location= $('#url_handle').val();
-   }
-   </script>
+
     </body>
+    
+
 </html>
