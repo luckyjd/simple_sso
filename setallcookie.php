679648d4a218ee9a63659e2ed8c9a813db633ba8
luckyjd <narusunday@gmail.com>
2018-01-22 14:23:35 +0700

    update

--- setallcookie.php
+++ setallcookie.php
@@ -9,7 +9,7 @@
 <body onload="loadComplete()">
    <body>
       <p>Please wait.....</p>
-      <img src="http://nhatt.learnphp.tinhvan.com/setcookie.php?theme=whateveryourthemehere" />
-      <img src="http://nhattx.sso.com.com/setcookie.php?theme=whateveryourthemehere" />
+      <img src="http://nhatt.learnphp.tinhvan.com/setcookie.php?theme=1" />
+      <img src="http://nhattx.sso.com/setcookie.php?theme=2" />
    </body>
 </html>
