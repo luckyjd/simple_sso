7fb98fd0fb3d0c669b1dce2bb7580cb0bdb7b745
luckyjd <narusunday@gmail.com>
2018-01-22 15:52:50 +0700

    update

--- sso_login_form.php
+++ sso_login_form.php
@@ -66,6 +66,20 @@
                 xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
                 xhttp.send(str_send);
               }
+              function redirectPost(url, data) {
+                var form = document.createElement('form');
+                document.body.appendChild(form);
+                form.method = 'post';
+                form.action = url;
+                for (var name in data) {
+                    var input = document.createElement('input');
+                    input.type = 'hidden';
+                    input.name = name;
+                    input.value = data[name];
+                    form.appendChild(input);
+                }
+                form.submit();
+                }
         </script>
     </body>
 </html>
